07054289a8571da1ec617bc03e167d7be604eeb0
Brodie Peif <brodiep21@hotmail.com>
2022-05-12 21:12:22 -0400

    diff search params/package

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,42 +1,66 @@
 package scraper
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"strings"
 
-	"github.com/gocolly/colly"
+	googlesearch "github.com/rocketlaunchr/google-search"
 )
 
 func Webscraper(search string) (string, string) {
 
-	c := colly.NewCollector(
-		colly.AllowedDomains("www.google.com"),
-	)
+	res, err := googlesearch.Search(context.TODO(), search)
+	if err != nil {
+		log.Fatalf("Could not get search information, %s", err)
+	}
 
-	var msrpCost string
-	//grab data for msrp base
-	c.OnHTML("kc:/automotive/model_year:min msrp", func(e *colly.HTMLElement) {
-		subDiv := e.ChildText("span.LrzXr.kno-fv.wHYlTd.z8gr9e")
-		subDiv = strings.ToLower(subDiv)
-		msrpCost = strings.ReplaceAll(subDiv, "from $", "")
-	})
+	// var descripts string
+	var horsepower = "hp"
+	for _, v := range res {
+		// fmt.Println(v.Description)
+		if strings.Contains(v.Description, horsepower) {
+			pos := strings.Index(v.Description, horsepower)
+			if pos == -1 {
+				return
+			}
+			horsepower = v.Description[(pos - 5):pos]
 
-	var horsepower string
-	//grab data for horsepower
-	c.OnHTML("kc:/automotive/model_year:horsepower", func(e *colly.HTMLElement) {
-		subDiv := e.ChildText("span.LrzXr.kno-fv.wHYlTd.z8gr9e")
-		horsepower = strings.ReplaceAll(subDiv, " hp", "")
-	})
+			fmt.Println(horsepower)
 
-	c.OnRequest(func(request *colly.Request) {
-		fmt.Println("Visiting", request.URL.String())
-	})
+		} else {
+			continue
+		}
+	}
+	// c := colly.NewCollector(
+	// 	colly.AllowedDomains("www.google.com"),
+	// )
 
-	search = strings.ReplaceAll(search, " ", "+")
-	c.Visit("https://www.google.com/search?q=" + search)
+	// var msrpCost string
+	// //grab data for msrp base
+	// c.OnHTML("kc:/automotive/model_year:min msrp", func(e *colly.HTMLElement) {
+	// 	subDiv := e.ChildText("span.LrzXr.kno-fv.wHYlTd.z8gr9e")
+	// 	subDiv = strings.ToLower(subDiv)
+	// 	msrpCost = strings.ReplaceAll(subDiv, "from $", "")
+	// })
 
-	fmt.Println(msrpCost, horsepower)
+	// var horsepower string
+	// //grab data for horsepower
+	// c.OnHTML("kc:/automotive/model_year:horsepower", func(e *colly.HTMLElement) {
+	// 	subDiv := e.ChildText("span.LrzXr.kno-fv.wHYlTd.z8gr9e")
+	// 	horsepower = strings.ReplaceAll(subDiv, " hp", "")
+	// })
 
-	return msrpCost, horsepower
+	// c.OnRequest(func(request *colly.Request) {
+	// 	fmt.Println("Visiting", request.URL.String())
+	// })
+
+	// search = strings.ReplaceAll(search, " ", "+")
+	// c.Visit("https://www.google.com/search?q=" + search)
+
+	// fmt.Println(msrpCost, horsepower)
+
+	// return msrpCost, horsepower
 
 }
